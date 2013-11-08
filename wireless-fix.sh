1dd9c545d74ab70b5fbfa31ca0f0d94d7b8dd469
Justin Edwards <justinledwards@gmail.com>
2013-11-08 16:36:34 -0600

    Update wireless-fix.sh

--- wireless-fix.sh
+++ wireless-fix.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# edit /etc/network/interfaces
+# change dhcp to static and manually enter your network configuration with a static ip
+# 
 mkdir -p /root/wirelessfix
 cd /root/wirelessfix
 
@@ -7,3 +10,7 @@ chmod +x bb-get-rcn-kernel-source.sh
 ./bb-get-rcn-kernel-source.sh 
 
 #todo add the rest
+# root@arm:/usr/local/lib# tar -czvf node_modules.tar.gz node_modules/
+# root@arm:~# tar -czvf npm.tar.gz .npm/
+#todo un gzip later
+
